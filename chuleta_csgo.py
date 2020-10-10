e9e980be97b786f15bd187f026f0655a0fad372b
shalooooo <gonzalopvargas@gmail.com>
2020-10-10 15:42:51 -0300

    Create chuleta_csgo.py

---
+++ chuleta_csgo.py
@@ -0,0 +1,25 @@
+### COMANDOS DE ENTRENAMIENTO ###
+
+# PACK TRAINING
+sv_cheats 1;bot_kick;sv_infinite_ammo 1;sv_grenade_trajectory 1;bind n noclip;mp_buy_anywhere 1;mp_buytime 60000;mp_roundtime_defuse 60;mp_roundtime_hostage 60;mp_roundtime 60;mp_restartgame 1
+
+# activar cheats
+sv_cheats 1
+
+# kickear a los bot
+bot_kick
+
+# municiones infinitas
+sv_infinite_ammo 1
+
+# ver trayectoria de granadas
+sv_grenade_trajectory 1
+
+# bindear tecla n para modo vuelo
+bind n noclip
+
+# tiempo de donra infinito para entrenamiento
+mp_roundtime_defuse 60;mp_roundtime_hostage 60;mp_roundtime 60;mp_restartgame 1
+
+# comprar donde quieras y en el tiempo que quieras
+mp_buy_anywhere 1;mp_buytime 60000
