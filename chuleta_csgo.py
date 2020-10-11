88c2486d69b7c75dbbe00887a168125cac76e213
shalooooo <gonzalopvargas@gmail.com>
2020-10-10 21:03:04 -0300

    Update chuleta_csgo.py

--- chuleta_csgo.py
+++ chuleta_csgo.py
@@ -18,8 +18,17 @@ sv_grenade_trajectory 1
 # bindear tecla n para modo vuelo
 bind n noclip
 
+# ver impactos de las balas
+sv_showimpacts 1;
+
 # tiempo de donra infinito para entrenamiento
 mp_roundtime_defuse 60;mp_roundtime_hostage 60;mp_roundtime 60;mp_restartgame 1
 
 # comprar donde quieras y en el tiempo que quieras
 mp_buy_anywhere 1;mp_buytime 60000
+
+# activa wall hack
+r_drawothermodels 2
+
+# situa un bot donde estes mirando
+bot_place
